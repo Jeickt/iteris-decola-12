c54646dafd8637c113981c994fb4d2e33ee46f5b
Taisa Braga <taisabraga.adm@gmail.com>
2022-03-19 12:44:33 -0300

    Criando página inicial

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import MeuHome from '../views/MeuHome.vue'
+import HomePage from '../views/HomePage.vue'
 
 Vue.use(VueRouter)
 
@@ -8,14 +8,14 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: MeuHome,
-  },
+    component: HomePage
+  }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes,
+  routes
 })
 
 export default router
