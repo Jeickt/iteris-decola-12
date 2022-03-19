7b2ae36c1a379a41be1ea97ab9cf94dfa35eb57f
Ana Paula Matos <paulylima3@gmail.com>
2022-03-19 13:47:11 -0300

    Happypassport- 1 versão

--- src/views/HappyPassport.vue
+++ src/views/HappyPassport.vue
@@ -1,13 +1,27 @@
 <template>
-    <h1>Lista</h1>
+  <div class="sucesso">
+    <p class="parabens"><strong>Parabéns!!!!</strong></p>
+    <p>
+      Você acabou de adquirir o passaporte da felicidade, aguardamos ansiosos
+      por você no dia 19/08.
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "ListaBlocos"
-}
+  name: "HappyPassport",
+};
 </script>
 
 <style scoped>
-
+.sucesso {
+  border: 1px solid black;
+  padding: 50px;
+  border-radius: 50%;
+}
+.parabens {
+  text-align: center;
+  margin-bottom: 10px;
+}
 </style>
