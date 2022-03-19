cf9e567daefeebde4b872b5716803765570f275f
Taisa Braga <taisabraga.adm@gmail.com>
2022-03-19 15:47:26 -0300

    Adicionando menu lateral

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,43 +1,26 @@
 <template>
 <div>
-  <h1 id="title" class="text-h2 text-center mb-3 mt-5">Happy Passaport</h1>
+  <h1 id="title" class="text-h2 text-center mb-3 mt-5">Happy Passport</h1>
   <p>O carnaval está chegando e nós vamos curtir juntos!!!
-O Happy passaport que te dar a oportunidade de curtir e se divertir na sua casa, com conforto, segurança e acessibilidade!
-Você e sua família, poderão curtir juntinhos esse bloco animado! </p>
+    O Happy Passport que te dar a oportunidade de curtir e se divertir na sua casa, com conforto, segurança e acessibilidade!
+    Você e sua família, poderão curtir juntinhos esse bloco animado!  
+
+    Com o Happy Passport você pode curtir os blocos de carnaval mais desejados do Brasil!
+    Ao Adiquirir o Happy Passaport, você terá o ingresso e o Kit maaais animiado desse carnaval! </p>
+    
   <router-link :to="{ name: 'ListaBlocos', params: { uf: name } }">
-      <v-btn class="mr-4" type="submit"> Submeter </v-btn>
+      <v-btn class="mr-4" type="submit"> Vamos Lá! </v-btn>
   </router-link>
-<v-carousel v-model="model">
-    <v-carousel-item
-      v-for="(color, i) in colors"
-      :key="color"
-    >
-      <v-sheet
-        :color="color"
-        height="100%"
-        tile
-      >
-        <v-row
-          class="fill-height"
-          align="center"
-          justify="center"
-        >
-          <div class="text-h2">
-            Slide {{ i + 1 }}
-          </div>
-        </v-row>
-      </v-sheet>
-    </v-carousel-item>
-  </v-carousel>
+  <img src="https://images.pexels.com/photos/1974100/pexels-photo-1974100.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
 </div>
 </template>
-
 
 <script>
 export default {
   data() {
     return {
       name: "PE",
+      
     };
   },
 };
