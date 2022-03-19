ca14967e6a3bcfa3acf77da419528b5b58342841
Taisa Braga <taisabraga.adm@gmail.com>
2022-03-19 13:20:48 -0300

    Criar e consertar componente AppTopBar

---
+++ src/components/AppTopBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card class="overflow-hidden">
+    <v-app-bar
+      absolute
+      color="#fcb69f"
+      dark
+      shrink-on-scroll
+      src="https://images.pexels.com/photos/796606/pexels-photo-796606.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+      scroll-target="#scrolling-techniques-2"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"  
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-app-bar-title class="title-home">Happy Passaport</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+    </v-app-bar>
+    <v-sheet
+      id="scrolling-techniques-2"
+      class="overflow-y-auto"
+      max-height="600"
+    >
+      <v-container></v-container>
+    </v-sheet>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.title-home{
+  color: #C51162;
+ 
+}
+</style>
